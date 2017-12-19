e68c66e432103f468d37e71a36c68dde212b0586
马添翼 <matianyi@jtoushou.com>
2017-12-19 14:25:38 +0800

    init project and add service

---
+++ src/main/java/cn/bdqn/shop/service/IdWorkerService.java
@@ -0,0 +1,12 @@
+package cn.bdqn.shop.service;
+
+/**
+ * IdWorkerService
+ */
+public interface IdWorkerService {
+    /**
+     * Generator Id
+     * @return long
+     */
+    public long getId();
+}
